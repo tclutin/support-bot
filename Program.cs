140c131ec29ea0e4c6e88e35b4d2d0b62488268b
Lutin <tclutin01@gmail.com>
2023-05-09 01:21:12 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+
+namespace SupportBot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            {
+                builder.Services.AddControllers();
+            }
+
+
+            var app = builder.Build();
+            {
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+
+                app.MapControllers();
+
+                app.Run();
+
+            }
+        }
+    }
+}

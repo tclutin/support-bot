49d082e8fd3a4a975c0ae7da26b6ac85f748cc90
Lutin <tclutin01@gmail.com>
2023-06-11 02:49:13 +0500

    add: check command 20%

---
+++ Commands/CheckCommand.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using SupportBot.Service;
+using SPIAPI;
+using Telegram.Bot.Types.ReplyMarkups;
+using System.Threading;
+using Telegram.Bot.Types.Enums;
+
+namespace SupportBot.Commands
+{
+    public class CheckCommand : ICommand
+    {
+        public TelegramBotClient Client => Bot.GetTelegramBot();
+
+        public string Name => "/check";
+
+        public async Task Execute(Update update, ISupportService supportService)
+        {
+            long chatId = update.Message.Chat.Id;
+
+            var user = await supportService.GetUserByTelegramIdAsync(chatId.ToString());
+            if (user == null) return;
+
+            if (user.TicketId == null)
+            {
+                await Client.SendTextMessageAsync(chatId, "В данный момент вы не имеете тикета.");
+                return;
+            }
+
+            var ticket = await supportService.GetTicketByIdAsync(user.TicketId);
+            if (ticket == null) return;
+
+            await Client.SendTextMessageAsync(chatId,
+                $"Ваш тикет:\nId: {ticket.Id}\nStatus: {ticket.Status}\nCreated: {ticket.CreatedDate}");
+        }
+    }
+}

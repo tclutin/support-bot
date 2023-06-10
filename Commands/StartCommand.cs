b8c8059858100c9a559a557d12f569f5c2b06b11
Lutin <tclutin01@gmail.com>
2023-06-11 02:20:29 +0500

    ref: start command

--- Commands/StartCommand.cs
+++ Commands/StartCommand.cs
@@ -1,6 +1,7 @@
 ﻿using SupportBot.Service;
 using Telegram.Bot.Types;
 using Telegram.Bot;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace SupportBot.Commands
 {
@@ -12,6 +13,25 @@ namespace SupportBot.Commands
 
         public async Task Execute(Update update, ISupportService supportService)
         {
+            long chatId = update.Message.Chat.Id;
+
+            var user = await supportService.GetUserByTelegramIdAsync(chatId.ToString());
+            if (user == null)
+            {
+                await supportService.CreateTelegramUserAsync(update.Message.Chat.Username, chatId.ToString());
+            }
+
+            var keyboard = new InlineKeyboardMarkup(new[]
+            {
+                new[]
+                {
+                 InlineKeyboardButton.WithCallbackData("Вызвать сотрудника", "call_support"),
+                }
+                });
+
+            await Client.SendTextMessageAsync(chatId,
+                       "Добро пожаловать. Вы попали в сервис технической поддержки. Нажмите кнопку, чтобы вызвать сотрудника поддержки для решения ваших вопросов.",
+            replyMarkup: keyboard);
         }
     }
 }
